100ad89a324c824cecf446f87b27aa37594abbe8
TamNhiDuong <yanxinyee@gmail.com>
2020-02-23 16:46:21 +0200

    done game

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class BoardComponent implements OnInit {
   private cells: string[] = [];
   private turn: string = '';
+  private winner: string = '';
+  private endGame: boolean;
 
   constructor() { }
 
@@ -20,10 +22,39 @@ export class BoardComponent implements OnInit {
       this.cells[i] = null;
     }
     this.turn = 'x';
+    this.endGame = false;
   }
 
   onMark(index) {
-    console.log(index)
+    console.log(index);
+      this.cells[index] = this.turn;
+      {this.turn == 'x' ? this.turn = 'o' : this.turn = 'x'};
+      this.checkGameWinner();
+  }
+
+  restart() {
+    this.startGame();
+  }
+
+  checkGameWinner() {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+    for (let line of lines) {
+      if (this.cells[line[0]] === this.cells[line[1]] 
+        && this.cells[line[1]] === this.cells[line[2]] 
+        && this.cells[line[0]] !== null) {
+          this.winner = this.cells[line[0]];
+          this.endGame = true; 
+        }
+    }
   }
 
 }
